620edab8d7a5f4069fa1636d0768bc6788f838ac
Daniel Blat <blatde91@gmail.com>
2017-09-29 21:24:30 -0400

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,48 @@
 var initialPage,
 	triviaHTML,
 	timeLeft = 30,
-	questions = ["question 1", "question 2", "question 3", "question 4", "question 5"],
-	answers = [["1a","1b","1c","1d"], ["2a","2b","2c","2d"],["3a","3b","3c","3d"],["4a","4b","4c","4d"],["5a","5b","5c","5d"]],
-	correctAnswers = ["A.1a","B.2b","C.3c","D.4d"],
+	questions = ["what year did the original blade runner release?", "who directed blade runner?", "What story is blade runner based on?", "which cut of the movie is considered canon?", "What year does blade runner take place?"],
+	answers = [["1982","1983","1980","1986"], ["steven spielberg","ridley scott","quentin tarantino","michael bay"],["i, robot","ubik","do androids dream of electric sheep?","dune"],["Theatrical Version","Director's Cut","Final cut","none of the above"],["2017","2019","2049","2325"]],
+	correctAnswers = ["a.1982","b.ridley scott","c.do androids dream of electric sheep?","c.Final cut","b.2019"],
 	gameProgress = 0,
 	correctCount = 0,
 	incorrectCount = 0,
 	unansweredCount = 0,
+	gifs = ["assets/images/harrisonford.gif", "assets/images/answerthequestion.gif", "assets/images/smoking.gif","assets/images/interrogation.gif", "assets/images/unicorn.gif"],
 	timer,
 	userGuess;
 
 function triviaGameHTML() {
-	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><h3>Time Remaining: <span id='timeRemaining'>" + timeLeft +"</span></h3></div><div id=currentQuestion><h3>"
-		+ questions[gameProgress] + "</h3></div><div class='answers'><h2 class='answerOpt'>A."
-		+ answers[gameProgress][0] + "</h2></div><div class='answers'><h2 class='answerOpt'>B."
-		+ answers[gameProgress][1] + "</h2></div><div class='answers'><h2 class='answerOpt'>C." 
-		+ answers[gameProgress][2] + "</h2></div><div class='answers'><h2 class='answerOpt'>D." 
-		+ answers[gameProgress][3] + "</h2></div>";
+	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><p>Time remaining: <span id='timeRemaining'>" + timeLeft +"</span></p><br></div><div id=currentQuestion><p>"
+		+ questions[gameProgress] + "</p><br></div><div class='answers'><p class='answerOpt'>a."
+		+ answers[gameProgress][0] + "</p><br></div><div class='answers'><p class='answerOpt'>b."
+		+ answers[gameProgress][1] + "</p><br></div><div class='answers'><p class='answerOpt'>c." 
+		+ answers[gameProgress][2] + "</p><br></div><div class='answers'><p class='answerOpt'>d." 
+		+ answers[gameProgress][3] + "</p><br></div>";
 	$("#main").html(triviaHTML);
 }
 
 function checkWin() {
 	correctCount++;
-	console.log(correctCount);
-	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><h3>Time Remaining: <span id='timeRemaining'>" + timeLeft +"</span></h3></div><div id=rightAnswer><h2>Correct!</h2></div>";
+	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><p>Time remaining: <span id='timeRemaining'>"
+		+ timeLeft +"</span></p></div><br><div id=rightAnswer><p>Correct!</p></div><br><div class='gifContainer'><img id='gif' src=" + gifs[gameProgress] +"></div>";
 	$("#main").html(triviaHTML);
 	setTimeout(gameState, 5000);
 }
 
 function timeoutLoss() {
 	unansweredCount++;
-	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><h3>Time Remaining: <span id='timeRemaining'>" + timeLeft + "</span></h3></div><h3>Out of Time</h3><div id=rightAnswer><h2>The Correct Answer was: " + correctAnswers[gameProgress] + "</h2></div>";
+	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><p>Time remaining: <span id='timeRemaining'>" 
+		+ timeLeft + "</span></p></div><br><p>Out of Time</p><br><div id=rightAnswer><p>The Correct Answer was: " + correctAnswers[gameProgress] + "</p></div><br><div class='gifContainer'><img id='gif' src=" + gifs[gameProgress] +"></div>";
 	$("#main").html(triviaHTML);
 	setTimeout(gameState, 5000);
 }
 
 function checkLoss() {
 	incorrectCount++;
-	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><h3>Time Remaining: <span id='timeRemaining'>" 
-		+ timeLeft + "</span></h3></div><h3>WRONG!</h3><div id=rightAnswer><h2>The Correct Answer was: " 
-		+ correctAnswers[gameProgress] + "</h2></div>";
+	triviaHTML = "<div id='triviaContainer'><div id='timeDisplay'><p>Time remaining: <span id='timeRemaining'>" 
+		+ timeLeft + "</span></p></div><br><p>WrONG!</p><br><div id=rightAnswer><p>The Correct Answer was: " 
+		+ correctAnswers[gameProgress] + "</p></div><br><div class='gifContainer'><img id='gif' src=" + gifs[gameProgress] +"></div>";
 	$("#main").html(triviaHTML);
 	setTimeout(gameState, 5000);
 }
@@ -61,11 +63,11 @@ function timerFunc(){
 }
 
 function gameOver(){
-	triviaHTML= "<div id='triviaContainer'><div id='timeDisplay'><h3>Time Remaining: <span id='timeRemaining'>"
-		+ timeLeft + "</span></h3></div><h3>Results!</h3><div id=resultsDisplay><h2>Correct Answers: <span>"
-		+ correctCount + "</span></h2><h2>Wrong Answers: <span>" 
-		+ incorrectCount +"</span></h2><h2>Time Outs: <span>" 
-		+ unansweredCount +"</span></h2></div><h2 id='restart'>Start Over?</h2>"
+	triviaHTML= "<div id='triviaContainer'><div id='timeDisplay'><p>Time remaining: <span id='timeRemaining'>"
+		+ timeLeft + "</span></p></div><br><p>results!</p><br><div id=resultsDisplay><p>Correct Answers: <span>"
+		+ correctCount + "</span></p><br><p>Wrong Answers: <span>" 
+		+ incorrectCount +"</span></p><br><p>Time Outs: <span>" 
+		+ unansweredCount +"</span></p></div><br><p id='restart'>Start Over?</p>"
 	$("#main").html(triviaHTML);
 }
 
@@ -95,13 +97,14 @@ function gameReset() {
 $(document).ready(function(){
 
 	function initialGameState() {
-		initialPage = "<div id='startingContainer'><button id='startButton'>START!</button></div>"
+		initialPage = "<div id='startingContainer'><div class='imgContainer'><img id='dragonball' src='assets/images/bladerunnerposter.jpg'>" 
+		+ "</div><div id='startgameContainer'><h1 id='startButton'>start!</h1></div></div>"
 		$("#main").html(initialPage);
 	}
 
 	initialGameState();
 
-	$("#startButton").on("click", function(event) {
+	$("#main").on("click", "#startButton", function(event) {
 		event.preventDefault();
 		triviaGameHTML();
 		timerFunc();
@@ -109,10 +112,8 @@ $(document).ready(function(){
 	})
 
 	$("#main").on("click", ".answerOpt", function(event) {
-		console.log("clicked")
 		userGuess = $(this).text();
 		if (userGuess === correctAnswers[gameProgress]) {
-			console.log("hi");
 			clearInterval(timer);
 			checkWin();
 		}
